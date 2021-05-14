61ef7cf1df660a6c235fc5801a089cbeb4bfa474
Cihat Solak <cihatsolak@hotmail.com>
2021-05-14 20:28:50 +0300

    Watermark.WebPublisher mvc project added

---
+++ NetCoreRabbitMQ/Watermark.WebPublisher/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Watermark.WebPublisher.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
